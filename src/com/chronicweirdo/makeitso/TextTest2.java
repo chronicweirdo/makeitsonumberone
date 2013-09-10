05d5510d09a7bd3acde629a1f4bc5020d5f856fe
Silviu Cacoveanu <silviu.cacoveanu@epfl.ch>
2013-09-10 16:10:18 +0200

    console text transformations

--- src/com/chronicweirdo/makeitso/TextTest2.java
+++ src/com/chronicweirdo/makeitso/TextTest2.java
@@ -1,5 +1,6 @@
 package com.chronicweirdo.makeitso;
 
+import java.awt.Point;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,65 +23,132 @@ public class TextTest2 {
 		}
 	}
 	
-	public static class TextTransformation {
-		public int x(int x) {
-			return 0;
+	public static interface TextTransformation {
+		Point destination(Point source); // get destination coordonates based on source coordonates
+		Point source(Point destination);
+	}
+	
+	public static class SplitByLinesTransformation implements TextTransformation {
+
+		private OriginalText sourceText;
+		private Text destinationText;
+		
+		@Override
+		public Point destination(Point source) {
+			int fi = source.x;
+			// count the number of new lines and distance from new line
+			int x = -1;
+			int y = 0;
+			int i = 0;
+			while (i < fi) {
+				if (sourceText.get(new Point(i, 0)) == '\n') {
+					y++;
+					x = -1;
+				}
+				x++;
+				i++;
+			}
+			return new Point(x, y);
 		}
-		public int y(int y) {
-			return 0;
+		
+		@Override
+		public Point source(Point destination) {
+			int y = destination.y;
+			int i = 0;
+			while (y > 0) {
+				if (sourceText.get(new Point(i, 0)) == '\n') {
+					y--;
+				}
+				i++;
+			}
+			i += destination.x;
+			return new Point(i, 0);
 		}
 	}
 	
 	public static interface Text {
-		void set(int x, int y, char c);
-		char get(int x, int y);
-		void insert(int x, int y, char c);		
-		void delete(int x, int y);
+		void set(Point p, char c);
+		char get(Point p);
+		void insert(Point p, char c);		
+		void delete(Point p);
 	}
 	public static class OriginalText implements Text {
-		private List<List<Character>> data;
-		
-		public void set(int x, int y, char c) {
-			data.get(y).set(x, c);
-		}
+		private List<Character> data;
 
-		public char get(int x, int y) {
-			return data.get(y).get(x);
-		}
-
-		public void insert(int x, int y, char c) {
-			data.get(y).add(x, c);
-		}
-
-		public void delete(int x, int y) {
-			data.get(y).remove(x);
-			if (data.get(y).size() == 0) {
-				data.remove(y);
+		public OriginalText(String string) {
+			data = new ArrayList<Character>();
+			for (Character c: string.toCharArray()) {
+				data.add(c);
 			}
 		}
 		
+		@Override
+		public void set(Point p, char c) {
+			// y is ignored
+			data.set(p.x, c);
+		}
+
+		@Override
+		public char get(Point p) {
+			// y is ignored
+			return data.get(p.x);
+		}
+
+		@Override
+		public void insert(Point p, char c) {
+			// y is ignored
+			data.add(p.x, c);
+		}
+
+		@Override
+		public void delete(Point p) {
+			// y is ignored
+			data.remove(p.x);
+		}
 	}
+	
 	public static class SourcedText implements Text {
 		Text source;
-		TextTransformation toSource;
+		TextTransformation transformation;
 
-		public void set(int x, int y, char c) {
-			
+		public void set(Point p, char c) {
+			source.set(transformation.source(p), c);
 		}
-		public char get(int x, int y){
-			return '.';
+		public char get(Point p){
+			return source.get(transformation.source(p));
 		}
-		public void insert(int x, int y, char c) {
-			
+		public void insert(Point p, char c) {
+			source.insert(transformation.source(p), c);
 		}
-		public void delete(int x, int y) {
-			
+		public void delete(Point p) {
+			source.delete(transformation.source(p));
 		}
 	}
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String text = "line one\n" +
+					  "second line";
+		/*
+		 *    00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29
+		 * 00  l  i  n  e     o  n  e \n  s  e  c  o  n  d     l  i  n  e
+		 * 
+		 *    00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29
+		 * 00  l  i  n  e     o  n  e \n
+		 * 01  s  e  c  o  n  d     l  i  n  e
+		 */
+		OriginalText original = new OriginalText(text);
+		
+		SplitByLinesTransformation transformation = new SplitByLinesTransformation();
+		transformation.sourceText = original;
+		
+		Point originalDestinationPoint = new Point(3, 1);
+		Point sourcePoint = transformation.source(originalDestinationPoint);
+		System.out.println(original.get(sourcePoint));
+		Point destinationPoint = transformation.destination(sourcePoint);
+		System.out.println(originalDestinationPoint);
+		System.out.println(sourcePoint);
+		System.out.println(destinationPoint);
+		
 	}
 
 }
