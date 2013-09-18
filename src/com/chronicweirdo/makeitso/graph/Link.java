37fab913ba694358ddcc9d1d0e840fb9e7109b3b
Silviu Cacoveanu <silviu.cacoveanu@epfl.ch>
2013-09-18 16:36:34 +0200

    graph modelling

---
+++ src/com/chronicweirdo/makeitso/graph/Link.java
@@ -0,0 +1,6 @@
+package com.chronicweirdo.makeitso.graph;
+
+public class Link {
+	private String type;
+	private String id;
+}
---
+++ src/com/chronicweirdo/makeitso/graph/Node.java
@@ -0,0 +1,8 @@
+package com.chronicweirdo.makeitso.graph;
+
+public class Node {
+
+	private String type;
+	private String id;
+	
+}
