695d4af4ee606403b885f6b97383110e4fa65219
Silviu Cacoveanu <silviu.cacoveanu@epfl.ch>
2013-10-04 14:54:24 +0200

    SPARQL search for history in semantic mediawiki

--- src/com/chronicweirdo/makeitso/ontology/JenaEngine.java
+++ src/com/chronicweirdo/makeitso/ontology/JenaEngine.java
@@ -19,6 +19,7 @@ import com.hp.hpl.jena.rdf.model.StmtIterator;
 import com.hp.hpl.jena.tdb.TDBFactory;
 import com.hp.hpl.jena.tdb.TDBLoader;
 import com.hp.hpl.jena.tdb.base.file.Location;
+import com.sun.org.apache.xalan.internal.xsltc.compiler.sym;
 
 /**
  * This class is responsible with:
@@ -51,6 +52,14 @@ public class JenaEngine {
 		return null;
 	}
 	
+	private static void writeResultSet(ResultSet results, StringBuilder builder) {
+		for (; results.hasNext();) {
+			QuerySolution soln = results.nextSolution();
+			builder.append(results.getRowNumber())
+				.append(". ").append(soln.toString())
+				.append("\n");
+		}
+	}
 	private static void writeResultSet(ResultSet results, String fileName) {
 		try { 
 			File file = new File(fileName);
@@ -58,14 +67,11 @@ public class JenaEngine {
 			if (!file.exists()) {
 				file.createNewFile();
 			}
- 
+			StringBuilder builder = new StringBuilder();
+			writeResultSet(results, builder);
 			FileWriter fw = new FileWriter(file.getAbsoluteFile());
 			BufferedWriter bw = new BufferedWriter(fw);
-			for (; results.hasNext();) {
-				QuerySolution soln = results.nextSolution();
-				bw.write(results.getRowNumber() + ". "
-						+ soln.toString() + "\n");
-			}
+			bw.write(builder.toString());
 			bw.close();
  
 		} catch (IOException e) {
@@ -153,6 +159,20 @@ public class JenaEngine {
 		//model.close();
 	}
 	
+	private static void executeRemoteSelectQuery(String url, String queryString, StringBuilder output) {
+		//Model model = TDBFactory.createDataset(directory).getDefaultModel();
+		
+		Query query = QueryFactory.create(queryString);
+		QueryExecution qexec = QueryExecutionFactory.sparqlService(url, query);
+		try {
+			writeResultSet(qexec.execSelect(), output);
+		} finally {
+			qexec.close();
+		}
+		
+		//model.close();
+	}
+	
 	private static void testLocal() {
 		String neurolexDB = "MyDatabases/DB1";
 		String slimsDB = "MyDatabases/DB2";
@@ -168,16 +188,38 @@ public class JenaEngine {
 	}
 	
 
+	public static String query1() {
+		return "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
+				"PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" +
+				"select *\n" +
+				"from <http://example.org/mediawikigraph>\n" +
+				"where {?s <http://example.org/mediawiki/index.php/Special:URIResolver/Property-3ATestproperty> ?o} LIMIT 100";
+	}
+	public static String query2() {
+		return "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
+				"PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" +
+				"select *\n" +
+				"from <http://example.org/mediawikigraph>\n" +
+				"where {" +
+				"	{<http://example.org/mediawiki/index.php/Special:URIResolver/Dummypage3> ?p ?o}"
+				+ " union"
+				+ " {?s ?p <http://example.org/mediawiki/index.php/Special:URIResolver/Dummypage3>}"
+				+ "} LIMIT 100";
+	}
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		//String url = "http://localhost:8000/sparql";
 		String url = "http://192.168.56.56:8890/sparql";
-		
+		String query = query2();
+		System.out.println(query);
+				
 		System.out.println("STARTING");
-		executeRemoteSelectQuery(url, "sparqlQuery2", "sparqlResult2");
+		StringBuilder builder = new StringBuilder();
+		executeRemoteSelectQuery(url, query, builder);
 		System.out.println("END");
+		System.out.println(builder.toString());
 	}
 
 }
