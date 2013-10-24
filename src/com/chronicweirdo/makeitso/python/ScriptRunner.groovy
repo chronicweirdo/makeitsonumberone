a676d27abc60ede6b8df7c1112078dce9ee40748
Silviu Cacoveanu <silviu.cacoveanu@epfl.ch>
2013-10-24 10:36:49 +0200

    execute python scripts from java (jython)

---
+++ src/com/chronicweirdo/makeitso/python/ScriptRunner.groovy
@@ -0,0 +1,49 @@
+package com.chronicweirdo.makeitso.python
+
+import java.nio.file.Path
+import java.nio.file.Paths
+
+import javax.script.ScriptEngine
+import javax.script.ScriptEngineManager
+
+import org.python.core.PyInteger
+import org.python.core.PyObject
+import org.python.util.PythonInterpreter
+
+class ScriptRunner {
+
+	static deflt() {
+		PythonInterpreter interp = new PythonInterpreter();
+		interp.exec("import sys");
+		interp.exec("print sys");
+		interp.set("a", new PyInteger(42));
+		interp.exec("print a");
+		interp.exec("x = 2+2");
+		PyObject x = interp.get("x");
+		System.out.println("x: " + x);
+	}
+	static jsr223() {
+		ScriptEngine engine = new ScriptEngineManager().getEngineByName("python");
+		engine.eval("import sys");
+		engine.eval("print sys");
+		engine.put("a", 42);
+		engine.eval("print a");
+		engine.eval("x = 2 + 2");
+		Object x = engine.get("x");
+		System.out.println("x: " + x);
+	}
+	static jsr223(Path path) {
+		ScriptEngine engine = new ScriptEngineManager().getEngineByName("python");
+		File file = path.toFile();
+		println file.getAbsolutePath()
+		if (file.exists()) {
+			engine.eval(file.newReader());
+		}
+	}
+	static main(args) {
+		//def();
+		//jsr223();
+		jsr223(Paths.get("src", "com", "chronicweirdo", "makeitso", "python", "test.py"));
+	}
+
+}
---
+++ src/com/chronicweirdo/makeitso/python/buildCache.py
@@ -0,0 +1,52 @@
+def getSub(txt,start,end):
+    rtn = list()
+    subobjects = [x.split(start)[1].split('\n') for x in txt.split(end) if start in x]
+    for subobject in subobjects:
+        temp = dict()
+        for line in subobject:
+            if u'=' in line and u'|' in line:
+                the_predicate,the_object = line.split(u'=')
+                the_predicate = the_predicate.split(u'|')[1]
+                temp[the_predicate] = the_object
+        if len(temp.keys()) != 0:
+            rtn.append(temp)
+    return rtn
+
+def getClass(txt,class_names):
+    for class_name in class_names:
+        if '{{'+class_name in txt: return class_name
+    return ''
+
+def getSubobjects(txt):
+    rtn = getSub(txt,'{{Single Instance Classification','}}')
+    rtn += getSub(txt,'{{Instance Relationship Classification','}}')
+    rtn += getSub(txt,'{{Single Instance Feature Measurement','}}')
+    rtn += getSub(txt,'{{Instance Relationship Feature Measurement','}}')
+    return rtn
+
+def buildSmartCache():
+    import mwclient
+    site = mwclient.Site('bbpfe01.epfl.ch', path='/project/labspace/')
+    user = 'bbpdaniboy@epfl.ch'
+    password = '9nHZ%6RD6d'
+    site.login(user, password )
+
+    rtn = dict()
+    the_classes = [
+        'Electrophysiological Cell Class',
+        'Morphological Cell Class',
+        'Molecular Cell Class',
+        'Location Class']
+    for x in the_classes: rtn[x] = list()
+
+    for page in site.pages:
+        title = page.name
+        text = page.edit()
+        rtn[title] = getSubobjects(text)
+        the_class = getClass(text,the_classes)
+        if the_class != '':rtn[the_class].append(title)
+
+    import pickle
+    f = open('new_cache.pkl','w');pickle.dump(rtn,f);f.close()
+
+buildSmartCache()
