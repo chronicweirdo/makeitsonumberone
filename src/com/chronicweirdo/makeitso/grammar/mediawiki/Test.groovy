24f7671235bbcfbddf5aad6e462eaac262f27f85
Silviu Cacoveanu <silviu.cacoveanu@epfl.ch>
2013-10-24 16:22:37 +0200

    mediawiki parsing: functional regex and antlr parser

--- src/com/chronicweirdo/makeitso/grammar/mediawiki/Test.groovy
+++ src/com/chronicweirdo/makeitso/grammar/mediawiki/Test.groovy
@@ -5,21 +5,32 @@ import java.nio.file.Paths
 import com.chronicweirdo.makeitso.grammar.Util
 
 class Test {
+	
+	private static final String REGEX_TEMPLATE = /(?m)(?s)\Q{{\E.+\Q}}\E/
+	private static final String PREDICATE_TEMPLATE = "template"
 
+	/**
+	 * Select interest regions in the text using a regex expression, then parse that structural
+	 * information using a certain predicate in the grammar.
+	 * @param text
+	 * @param regex
+	 * @param predicate
+	 * @return
+	 */
+	static parse(text, regex, predicate) {
+		def matcher = text =~ regex
+		MediawikiListenerImpl listener = new MediawikiListenerImpl();
+		for (match in matcher) {
+			Util.test(MediawikiLexer.class, MediawikiParser.class,
+				listener, predicate, match);
+		}
+	}
+	
 	static main(args) {
 		String path = Paths.get("src", "com", "chronicweirdo", "makeitso",
 			"grammar", "mediawiki", "test1").toString();
 		String file = Util.readFile(path);
-		def matcher = file =~ /(?m)(?s)\Q{{\E.+\Q}}\E/
-		for (match in matcher) {
-			println match
-			Util.test(MediawikiLexer.class, MediawikiParser.class,
-				new MediawikiListenerImpl(), "template",
-				match);
-		}
-		/*Util.test(MediawikiLexer.class, MediawikiParser.class,
-			new MediawikiListenerImpl(), "page",
-			file);*/
+		parse(file, REGEX_TEMPLATE, PREDICATE_TEMPLATE);
 	}
 
 }
