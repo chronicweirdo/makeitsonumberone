0d5cfdce118afefab6356ef0e933d5de8a09ceaf
unknown <scacoveanu@LUW7SCACOVEANU.nt.ingenuity.com>
2014-04-24 15:02:05 +0300

    naframa project creation

---
+++ naframa/src/main/java/com/chronicweirdo/naframa/App.java
@@ -0,0 +1,13 @@
+package com.chronicweirdo.naframa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ naframa/src/main/java/com/chronicweirdo/naframa/DoImage.java
@@ -0,0 +1,41 @@
+package com.chronicweirdo.naframa;
+
+import org.apache.batik.transcoder.TranscoderException;
+import org.apache.batik.transcoder.TranscoderInput;
+import org.apache.batik.transcoder.TranscoderOutput;
+import org.apache.batik.transcoder.image.JPEGTranscoder;
+import org.apache.log4j.Logger;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+/**
+ * Created by scacoveanu on 4/24/2014.
+ */
+public class DoImage {
+
+    private static final Logger log = Logger.getLogger(DoImage.class);
+
+    public static void main(String[] args) throws IOException, TranscoderException {
+
+        String fileName = "%USERPROFILE%\\Dropbox\\mydata\\drawing\\povesti degeaba\\capra.svg";
+
+        // create a JPEG transcoder
+        JPEGTranscoder t = new JPEGTranscoder();
+        // set hints
+        t.addTranscodingHint(JPEGTranscoder.KEY_QUALITY, new Float(.8));
+
+        String svgURI = new File(fileName).toURL().toString();
+        TranscoderInput input = new TranscoderInput(svgURI);
+
+        OutputStream ostream = new FileOutputStream("out.jpg");
+        TranscoderOutput output = new TranscoderOutput(ostream);
+
+        t.transcode(input, output);
+
+        ostream.flush();
+        ostream.close();
+    }
+}
