4450efd1a77a09bc925d6ae45b67efff9079fd70
chronicweirdo <chronicweirdo@gmail.com>
2014-05-22 12:16:31 +0300

    tdd: some more hard-coding to make the test pass

--- tdd/src/main/java/org/chronicweirdo/tdd/Template.java
+++ tdd/src/main/java/org/chronicweirdo/tdd/Template.java
@@ -4,15 +4,18 @@ package org.chronicweirdo.tdd;
  * Created by scacoveanu on 5/22/2014.
  */
 public class Template {
+
+    private String variableValue;
+
     public Template(String templateText) {
 
     }
 
     public void set(String variable, String value) {
-
+        this.variableValue = value;
     }
 
     public String evaluate() {
-        return "Hello, Reader";
+        return "Hello, " + variableValue;
     }
 }
