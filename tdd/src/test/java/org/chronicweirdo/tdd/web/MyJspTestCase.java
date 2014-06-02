165647e67e5cf0aa9d626393c65f41c90ecd740c
chronicweirdo <chronicweirdo@gmail.com>
2014-06-02 15:14:29 +0300

    tdd: first jsp test - red

---
+++ tdd/src/test/java/org/chronicweirdo/tdd/web/MyJspTestCase.java
@@ -0,0 +1,14 @@
+package org.chronicweirdo.tdd.web;
+
+import net.sf.jsptest.HtmlTestCase;
+
+/**
+ * Created by scacoveanu on 6/2/2014.
+ */
+public class MyJspTestCase extends HtmlTestCase {
+
+    @Override
+    protected String getWebRoot() {
+        return "./websrc/jsp";
+    }
+}
---
+++ tdd/src/test/java/org/chronicweirdo/tdd/web/TestLoginPage.java
@@ -0,0 +1,14 @@
+package org.chronicweirdo.tdd.web;
+
+/**
+ * Created by scacoveanu on 6/2/2014.
+ */
+public class TestLoginPage extends MyJspTestCase {
+
+    public void testFormFieldsArePresent() throws Exception {
+        get("/login.jsp");
+        form().shouldHaveField("j_username");
+        form().shouldHaveField("j_password");
+        form().shouldHaveSubmitButton("login");
+    }
+}
