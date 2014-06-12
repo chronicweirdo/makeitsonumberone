2e3d0ba8c5ad997b4f2b5d9ec84ca116db6fa440
chronicweirdo <chronicweirdo@gmail.com>
2014-06-12 13:03:03 +0300

    creating empetrei project

---
+++ empetrei/src/main/java/org/chronicweirdo/empetrei/App.java
@@ -0,0 +1,13 @@
+package org.chronicweirdo.empetrei;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
