45045967a21bab8ae452178741689d829dd64c29
chronicweirdo <chronicweirdo@gmail.com>
2014-06-22 18:27:53 +0300

    tdd: concurrency counter test for thread safety - not thread safe

--- tdd/src/test/java/org/chronicweirdo/tdd/concurrent/CounterTest.java
+++ tdd/src/test/java/org/chronicweirdo/tdd/concurrent/CounterTest.java
@@ -17,4 +17,25 @@ public class CounterTest {
         counter.increment();
         assertEquals(2, counter.value());
     }
+
+    @Test
+    public void testForThreadSafety() throws Exception {
+        final Counter codeUnderTest = new Counter();
+        final int numberOfThreads = 20;
+        final int incrementsPerThread = 100;
+
+        Runnable runnable = new Runnable() {
+            public void run() {
+                for (int i = 0; i < incrementsPerThread; i++) {
+                    codeUnderTest.increment();
+                }
+            }
+        };
+
+        for (int i = 0; i < numberOfThreads; i++) {
+            new Thread(runnable).start();
+        }
+        Thread.sleep(500);
+        assertEquals(numberOfThreads * incrementsPerThread, codeUnderTest.value());
+    }
 }
