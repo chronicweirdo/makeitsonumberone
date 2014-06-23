cb3d2d06a8b6811dfdd2a2729ae0814afbb80e70
chronicweirdo <chronicweirdo@gmail.com>
2014-06-23 12:07:18 +0300

    spring web application with abstract command controller

---
+++ springweb/src/main/java/com/chronicweirdo/springweb/command/APICommand.java
@@ -0,0 +1,17 @@
+package com.chronicweirdo.springweb.command;
+
+/**
+ * Created by scacoveanu on 6/23/2014.
+ */
+public class APICommand {
+
+    private String methodName;
+
+    public String getMethodName() {
+        return methodName;
+    }
+
+    public void setMethodName(String methodName) {
+        this.methodName = methodName;
+    }
+}
---
+++ springweb/src/main/java/com/chronicweirdo/springweb/controller/APIController.java
@@ -0,0 +1,36 @@
+package com.chronicweirdo.springweb.controller;
+
+import com.chronicweirdo.springweb.command.APICommand;
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractCommandController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+/**
+ * Created by scacoveanu on 6/23/2014.
+ */
+@Controller
+@RequestMapping("/api")
+public class APIController extends AbstractCommandController {
+
+    private static final Logger log = Logger.getLogger(APIController.class);
+
+    public APIController() {
+        setCommandClass(APICommand.class);
+    }
+
+    @Override
+    protected ModelAndView handle(HttpServletRequest request, HttpServletResponse httpServletResponse, Object command, BindException e) throws Exception {
+        APICommand apiCommand = (APICommand) command;
+        log.info("command method name: " + apiCommand.getMethodName());
+
+        return new ModelAndView("api");
+    }
+}
