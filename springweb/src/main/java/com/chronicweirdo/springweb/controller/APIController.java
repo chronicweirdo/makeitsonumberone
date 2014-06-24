7038188f6d6832bd797fe2786522d11128b66613
chronicweirdo <chronicweirdo@gmail.com>
2014-06-24 10:34:49 +0300

    tomcat server test update logs

--- springweb/src/main/java/com/chronicweirdo/springweb/controller/APIController.java
+++ springweb/src/main/java/com/chronicweirdo/springweb/controller/APIController.java
@@ -11,6 +11,8 @@ import org.springframework.web.servlet.mvc.AbstractCommandController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.IOException;
 
 
 /**
@@ -29,13 +31,46 @@ public class APIController extends AbstractCommandController {
     @Override
     protected ModelAndView handle(HttpServletRequest request, HttpServletResponse httpServletResponse, Object command, BindException e) throws Exception {
         APICommand apiCommand = (APICommand) command;
+
         log.info("command method name: " + apiCommand.getMethodName());
         if (apiCommand.getGeneId() != null) {
             log.info("gene ids size: " + apiCommand.getGeneId().length);
         } else {
             log.info("gene ids are null");
         }
-
+        logRequest(request);
         return new ModelAndView("api");
+    }
+
+    private void logRequest(HttpServletRequest request) {
+        try {
+            log.info(">>>>>>> request is null: " + (request == null));
+            log.info(">>>>>>> request object class: " + request.getClass().getName());
+            log.info(">>>>>>> request content lenght: " + request.getContentLength());
+            String[] parameterValues = request.getParameterValues("geneId");
+            if (parameterValues == null) {
+                log.info(">>>>> gene id parameter values on request are null");
+            } else {
+                log.info(">>>>>>> gene id parameter values on request size: " + parameterValues.length);
+            }
+            StringBuilder builder = new StringBuilder();
+            String line;
+            BufferedReader reader = null;
+
+            reader = request.getReader();
+
+            while ((line = reader.readLine()) != null) {
+                builder.append(line);
+            }
+            log.info(">>>>>>>>> request builder length: " + builder.length());
+            if (builder.length() > 200) {
+                log.info(">>>>>>>> request end: " + builder.substring(builder.length() - 200));
+            } else {
+                log.info(">>>>>>>> request: " + builder.toString());
+            }
+        } catch (IOException e) {
+            log.error(e, e);
+            log.info(">>>>>>>>> error: " + e.getMessage());
+        }
     }
 }
