2e724d93c6fe2f8ec19ad04d62be9fcea03f72aa
chronicweirdo <chronicweirdo@gmail.com>
2014-07-07 09:29:08 +0300

    puzzlers

--- puzzlers/src/main/java/org/chronicweirdo/javapuzzlers/chapter1/puzzle6/Multicast.java
+++ puzzlers/src/main/java/org/chronicweirdo/javapuzzlers/chapter1/puzzle6/Multicast.java
@@ -5,6 +5,9 @@ public class Multicast {
         System.out.println((int) (char) (byte) -1); // will print 129 (binary 10000001)- wrong!
         /*
         Behavior depends on sign extension behavior of casts.
+        To control this behavior, better just use a bit mask:
          */
+        char c = (char) (byte) -1;
+        int i = c & 0xffff;
     }
 }
