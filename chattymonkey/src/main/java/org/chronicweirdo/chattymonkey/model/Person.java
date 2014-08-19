616921d21956ed82bdc02d4d4572bbb9486328f3
chronicweirdo <chronicweirdo@gmail.com>
2014-08-19 11:40:56 +0300

    cm: setting up tests

---
+++ chattymonkey/src/main/java/org/chronicweirdo/chattymonkey/model/Person.java
@@ -0,0 +1,35 @@
+package org.chronicweirdo.chattymonkey.model;
+
+/**
+ * Created by scacoveanu on 8/19/2014.
+ */
+public class Person {
+
+    private String userName;
+    private String password;
+    private String email;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
---
+++ chattymonkey/src/test/java/org/chronicweirdo/chattymonkey/model/PersonTest.java
@@ -0,0 +1,19 @@
+package org.chronicweirdo.chattymonkey.model;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Created by scacoveanu on 8/19/2014.
+ */
+public class PersonTest {
+
+    @Test
+    public void createAPerson() throws Exception {
+        Person person = new Person();
+        person.setEmail("person@email.com");
+        person.setUserName("nick");
+        person.setPassword("p455word");
+        assertNotNull(person);
+    }
+}
