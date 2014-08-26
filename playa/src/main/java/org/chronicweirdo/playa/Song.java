3dab506b07c399b072fc32dcd14e1e9d3ac696e8
chronicweirdo <chronicweirdo@gmail.com>
2014-08-26 10:46:10 +0300

    playa project

---
+++ playa/src/main/java/org/chronicweirdo/playa/Song.java
@@ -0,0 +1,13 @@
+package org.chronicweirdo.playa;
+
+import java.util.List;
+
+/**
+ * Created by scacoveanu on 8/26/2014.
+ */
+public class Song {
+
+    private List<Tag> tags;
+
+    
+}
---
+++ playa/src/main/java/org/chronicweirdo/playa/Tag.java
@@ -0,0 +1,21 @@
+package org.chronicweirdo.playa;
+
+/**
+ * Created by scacoveanu on 8/26/2014.
+ */
+public class Tag {
+
+    private String text;
+
+    public Tag(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
