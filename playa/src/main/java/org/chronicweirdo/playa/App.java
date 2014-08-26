94dc3c0d793f7b54a6c16cf41086cb750a05df06
chronicweirdo <chronicweirdo@gmail.com>
2014-08-26 10:43:59 +0300

    playa project

---
+++ playa/src/main/java/org/chronicweirdo/playa/App.java
@@ -0,0 +1,13 @@
+package org.chronicweirdo.playa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
