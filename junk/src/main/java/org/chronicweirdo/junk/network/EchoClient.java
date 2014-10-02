c925a53b438ae4f0e718e1c36f2c172a1f6023ce
chronicweirdo <chronicweirdo@gmail.com>
2014-10-02 13:24:57 +0300

    simple network ping

---
+++ junk/src/main/java/org/chronicweirdo/junk/network/EchoClient.java
@@ -0,0 +1,37 @@
+package org.chronicweirdo.junk.network;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+/**
+ * Created by scacoveanu on 10/2/2014.
+ */
+public class EchoClient {
+
+    public void start() {
+        String host = "localhost";
+        int port = EchoServer.PORT;
+
+        try {
+            Socket echoSocket = new Socket(host, port);
+
+            PrintWriter out = new PrintWriter(echoSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(echoSocket.getInputStream()));
+
+            String message = "message";
+            out.println(message);
+            String returnLine;
+            while ((returnLine = in.readLine()) != null) {
+                System.out.println("[client] " + returnLine);
+            }
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ junk/src/main/java/org/chronicweirdo/junk/network/EchoServer.java
@@ -0,0 +1,39 @@
+package org.chronicweirdo.junk.network;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * Created by scacoveanu on 10/2/2014.
+ */
+public class EchoServer implements Runnable {
+
+    public static final int PORT = 13542;
+
+    @Override
+    public void run() {
+        start();
+    }
+
+    public void start() {
+        try {
+            ServerSocket serverSocket = new ServerSocket(PORT);
+            Socket clientSocket = serverSocket.accept();
+
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+                System.out.println("[server] " + inputLine);
+                out.println(inputLine);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
