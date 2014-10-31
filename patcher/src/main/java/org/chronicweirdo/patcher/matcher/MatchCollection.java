af5dd630e352c877e2b36587cf5d53eafbe76f45
chronicweirdo <chronicweirdo@gmail.com>
2014-10-31 15:42:52 +0200

    matcher must be rewritten

---
+++ patcher/src/main/java/org/chronicweirdo/patcher/matcher/MatchCollection.java
@@ -0,0 +1,22 @@
+package org.chronicweirdo.patcher.matcher;
+
+import org.chronicweirdo.patcher.scanner.Entry;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by scacoveanu on 10/31/2014.
+ */
+public class MatchCollection {
+
+    private List<Entry> matches = new ArrayList<Entry>();
+
+    public void addMatch(Entry newEntry) {
+        // if there is no match, add it
+        // if there is a match
+        //      check the score of the match
+        //      if it is larger than the score of the existing matches, remove all matches and keep the new one
+        //      if it is the same as the score of the existing matches,
+    }
+}
