f229f904cc340729468bf6095258446aa8f89947
chronicweirdo <chronicweirdo@gmail.com>
2014-12-28 20:35:12 +0200

    starting folder servlet

---
+++ dump/src/main/java/org/chronicweirdo/dump/web/FolderServlet.java
@@ -0,0 +1,10 @@
+package org.chronicweirdo.dump.web;
+
+/**
+ * This servlet scans some folder and offers the files in that folder.
+ *
+ * Created by Silviu on 12/26/2014.
+ */
+public class FolderServlet {
+    
+}
