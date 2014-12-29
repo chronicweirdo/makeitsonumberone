7454469c5695999fe8d6b16eb4d324361bdf096a
Silviu Cacoveanu <scacoveanu@ingenuity.com>
2014-12-29 15:32:10 +0200

    starting templating

---
+++ dump/src/test/java/org/chronicweirdo/dump/TemplatingTest.java
@@ -0,0 +1,17 @@
+package org.chronicweirdo.dump;
+
+import org.junit.Test;
+
+/**
+ * Created by scacoveanu on 12/29/2014.
+ *
+ * http://blog.stackhunter.com/2014/01/17/10-reasons-to-replace-your-jsps-with-freemarker-templates/
+ */
+public class TemplatingTest {
+
+
+    @Test
+    public void templatePlusModel() throws Exception {
+        
+    }
+}
